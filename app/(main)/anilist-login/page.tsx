978d1f9bf9fd73f2ec61cf59a0ba2fa31dfa5dc6
Sam Golpasand <samgolpasand45@gmail.com>
2025-03-25 07:09:14 +0100

    Started on anilist auth

---
+++ app/(main)/anilist-login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div >
+      <a href='https://anilist.co/api/v2/oauth/authorize?client_id=25111&response_type=token'>Login with AniList</a>
+    </div>
+  )
+}
